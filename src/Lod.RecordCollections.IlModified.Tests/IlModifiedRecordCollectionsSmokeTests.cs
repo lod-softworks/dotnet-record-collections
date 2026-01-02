61f48aaa8150dad934c9372ea14bf92c40945490
Zach Cutler <36868618+linkofdarkness@users.noreply.github.com>
2026-01-02 11:29:25 -0700

    test: additional collection tests

--- src/Lod.RecordCollections.IlModified.Tests/IlModifiedRecordCollectionsSmokeTests.cs
+++ src/Lod.RecordCollections.IlModified.Tests/IlModifiedRecordCollectionsSmokeTests.cs
@@ -4,9 +4,30 @@ namespace Lod.RecordCollections.IlModified.Tests;
 public class IlModifiedRecordCollectionsSmokeTests
 {
     [TestMethod]
+    public void WithExpression_ClonesRecordDictionary()
+    {
+        var original = new RecordDictionary<int, int>
+        {
+            [1] = 10,
+            [2] = 20,
+        };
+
+        // This only compiles if the referenced binary is record-compliant (i.e., IL-modified).
+        var clone = original with { };
+
+        Assert.IsTrue(original.Equals(clone));
+        Assert.AreNotSame(original, clone);
+        Assert.HasCount(original.Count, clone);
+
+        clone[3] = 30;
+        Assert.HasCount(2, original);
+        Assert.HasCount(3, clone);
+    }
+
+    [TestMethod]
     public void WithExpression_ClonesRecordList()
     {
-        var original = new RecordList<int>(new[] { 1, 2, 3 });
+        var original = new RecordList<int>([1, 2, 3,]);
 
         // This only compiles if the referenced binary is record-compliant (i.e., IL-modified).
         var clone = original with { };
@@ -21,22 +42,53 @@ public class IlModifiedRecordCollectionsSmokeTests
     }
 
     [TestMethod]
-    public void WithExpression_ClonesRecordDictionary()
+    public void WithExpression_ClonesRecordQueue()
     {
-        var original = new RecordDictionary<int, int>
-        {
-            [1] = 10,
-            [2] = 20,
-        };
+        var original = new RecordQueue<int>([1, 2, 3,]);
 
+        // This only compiles if the referenced binary is record-compliant (i.e., IL-modified).
         var clone = original with { };
 
+        CollectionAssert.AreEqual(original, clone);
         Assert.IsTrue(original.Equals(clone));
         Assert.AreNotSame(original, clone);
-        Assert.HasCount(original.Count, clone);
 
-        clone[3] = 30;
-        Assert.HasCount(2, original);
-        Assert.HasCount(3, clone);
+        clone.Enqueue(4);
+        Assert.HasCount(3, original);
+        Assert.HasCount(4, clone);
+    }
+
+    [TestMethod]
+    public void WithExpression_ClonesRecordSet()
+    {
+        var original = new RecordSet<int>([1, 2, 3,]);
+
+        // This only compiles if the referenced binary is record-compliant (i.e., IL-modified).
+        var clone = original with { };
+
+        CollectionAssert.AreEqual(original, clone);
+        Assert.IsTrue(original.Equals(clone));
+        Assert.AreNotSame(original, clone);
+
+        clone.Add(4);
+        Assert.HasCount(3, original);
+        Assert.HasCount(4, clone);
+    }
+
+    [TestMethod]
+    public void WithExpression_ClonesRecordStack()
+    {
+        var original = new RecordStack<int>([1, 2, 3,]);
+
+        // This only compiles if the referenced binary is record-compliant (i.e., IL-modified).
+        var clone = original with { };
+
+        CollectionAssert.AreEqual(original, clone);
+        Assert.IsTrue(original.Equals(clone));
+        Assert.AreNotSame(original, clone);
+
+        clone.Push(4);
+        Assert.HasCount(3, original);
+        Assert.HasCount(4, clone);
     }
 }
